13af00713304ded9ad1de92cea4284d56f0f07d2
SimonROZEC <simonrozec44@gmail.com>
2020-05-05 22:33:25 +0200

    Prevent attack/defense during Gobbit! situations

--- src/gamestates/action-handlers/AttackHandler.gd
+++ src/gamestates/action-handlers/AttackHandler.gd
@@ -5,8 +5,11 @@ extends ActionHandler
 # Handles an attack from the current client on the target deck
 func handle_attack(target: Deck) -> void:
 	var deck : Deck = NetworkManager.me().played_cards.get_ref()
-	if target.empty() or deck.empty():
+	# No attacks when a Gobbit is active
+	if target.empty() or deck.empty() or \
+			turn.gobbit_handler.check_gobbit_active():
 		return
+
 	
 	if check_attack_valid(deck, target):
 		if is_killing_attack(deck, target):
@@ -19,8 +22,7 @@ func handle_attack(target: Deck) -> void:
 
 
 # Checks if an attack from deck to the target deck is valid
-# TODO: Exclude the case where there is a possible Gobbit! to play
-func check_attack_valid(deck: Deck, target: Deck) -> bool:
+func check_attack_valid(deck: Deck, target: Deck) -> bool:	
 	var top_card := deck.get_card_on_top()
 	var top_target := target.get_card_on_top()
 	return top_card.eats(top_target)
--- src/gamestates/action-handlers/DefenseHandler.gd
+++ src/gamestates/action-handlers/DefenseHandler.gd
@@ -4,7 +4,8 @@ extends ActionHandler
 # Handles an attack from the current client on the target deck
 func handle_defense() -> void:
 	var deck : Deck = NetworkManager.me().played_cards.get_ref()
-	if deck.empty():
+	if deck.empty() or \
+			turn.gobbit_handler.check_gobbit_active():
 		return
 	
 	if check_defense_valid():
@@ -15,7 +16,6 @@ func handle_defense() -> void:
 
 # Checks if a defense is justified
 # TODO: Handle spirit defense (add a "lost" property in players)
-# TODO: Handle gobbit rule
 func check_defense_valid() -> bool:
 	var deck : Deck = NetworkManager.me().played_cards.get_ref()
 	var self_card := deck.get_card_on_top()
