44cc825c809898f0227c50a05558376c1bb09af9
SimonROZEC <simonrozec44@gmail.com>
2020-07-04 23:44:31 +0200

    Create basis of the arbitration system

--- src/game/Arbitrator.gd
+++ src/game/Arbitrator.gd
@@ -3,9 +3,14 @@ class_name Arbitrator
 # Arbitrates each action taking the ping in account
 
 signal freed # The arbitrator was just freed form processing actions 
+signal action_arbitrated(source, target)
+
+var timeout := 0.3 # TODO: Make this equal to max ping among players
 
 var update_times := {} # Last update times of the players played card clientside
-var action_buffer := []
+var action_buffer := {} # Buffer of pending player actions
+var timers := {}
+var free := true # Is the arbitrator busy or not
 
 
 func _init(played_cards: Array) -> void:
@@ -20,12 +25,47 @@ func is_free() -> bool:
 	return action_buffer.empty()
 
 
+# Free the action buffer
+sync func free_action_buffer() -> void:
+	free = true
+
+
+# Tells the server to buffer an action and sets the arbitrator as busy clientside
 func register_action(source: int, target: int, time: int) -> void:
 	rpc_id(1, "_buffer_action", source, target)
+	free = false
 
 
 master func _buffer_action(source: int, target: int, time: int) -> void:
-	pass
+	var action = Action.new(source, target, time)
+	if not target in action_buffer:
+		action_buffer[target] = []
+	action_buffer[target].append(action)
+	
+	# Setup timeout
+	var timer := get_tree().create_timer(timeout)
+	timers[target] = timer
+	timer.connect("timeout", self, "_on_timer_timeout", [target])
+	free = false
+
+
+func _on_timer_timeout(target: int) -> void:
+	# Get fastest action
+	var fastest : Action = null
+	var buffer = action_buffer[target]
+	for action in buffer:
+		if fastest == null:
+			fastest = action
+		elif fastest.time > action.time:
+			fastest = action
+	# BUG: Take into account faster errors from players
+	emit_signal("action_arbitrated", fastest.source, fastest.target)
+	
+	action_buffer.erase(target)
+	timers.erase(target)
+	if action_buffer.empty():
+		rpc("free_action_buffer")
+	
 
 
 func _on_deck_updated(deck: Deck, card: Card) -> void:
@@ -38,3 +78,8 @@ class Action:
 	var source : int
 	var target : int
 	var time: int # The reaction time in milliseconds
+	
+	func _init(s: int, t: int, tm: int) -> void:
+		source = s
+		target = t
+		time = tm
